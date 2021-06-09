8a7ad092f61cf6229d5df98e5655ee765112c1dc
fotomelu <69889253+fotomelu@users.noreply.github.com>
2021-06-08 17:04:55 -0700

    eslint fix errors

--- js/app.js
+++ js/app.js
@@ -39,71 +39,71 @@ function plusSlides(steps) {
 
 function trending(num) {
 
-    const trendingElements = document.getElementsByClassName('slideshow-container')
-    const trendingEl = trendingElements[0] 
-    const path_trending = `https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=${num}` 
+    const trendingElements = document.getElementsByClassName('slideshow-container');
+    const trendingEl = trendingElements[0]; 
+    const path_trending = `https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=${num}`; 
 
     fetch (path_trending).then(function (res) { 
-        return res.json()
+        return res.json();
     }).then(function(json){
-        console.log(json.data[0].images.fixed_width.url)
-        let resultsTrending = ''
+        console.log(json.data[0].images.fixed_width.url);
+        let resultsTrending = '';
     
         json.data.forEach(function(obj) {
-            console.log(obj)
+            console.log(obj);
 
-            const url = obj.images.fixed_width.url
-            const title = obj.title
+            const url = obj.images.fixed_width.url;
+            const title = obj.title;
             
             //To add strings
             
             resultsTrending += `<div class="mySlides fade">
                     <img src="${url}"  style= "width: 357px; height: 275px" alt="${title}">
-                </div>`
+                </div>`;
             slideButtons = `<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-            <a class ="next" onclick="plusSlides(1)">&#10095;</a>`
+            <a class ="next" onclick="plusSlides(1)">&#10095;</a>`;
         });
             
-        console.log(resultsTrending)
-        trendingEl.innerHTML = resultsTrending + slideButtons //to introduce in html
+        console.log(resultsTrending);
+        trendingEl.innerHTML = resultsTrending + slideButtons; //to introduce in html
  
         showTrending(slideIndex);
 
     }).catch(function(err) {
-        console.log(err.message)
-    })
+        console.log(err.message);
+    });
 }
 
 //API Conection Search
 
-const searchForm = document.getElementById('search-form')
-const searchInput = document.getElementById('search-input')
-const resultsEl = document.getElementById('results')
+const searchForm = document.getElementById('search-form');
+const searchInput = document.getElementById('search-input');
+const resultsEl = document.getElementById('results');
 
 //Event Listener to submit
 searchForm.addEventListener('submit',function(e) {
-    e.preventDefault()
-    const q = searchInput.value
-    search(q)
-})
+    e.preventDefault();
+    const q = searchInput.value;
+    search(q);
+});
 
 function search(query) {
    
-    const path_search = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&limit=15&q=${query}`
+    const path_search = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&limit=15&q=${query}`;
 
     fetch (path_search).then(function (res) { 
-        return res.json()
+        return res.json();
     }).then(function(json){
-        console.log(json.data[0].images.fixed_width.url)
-        let resultsHTML = ''
+        console.log(json.data[0].images.fixed_width.url);
+        let resultsHTML = '';
     
         json.data.forEach(function(obj) {
-            console.log(obj)
+            console.log(obj);
 
-            const url = obj.images.fixed_width.url
-            const width = obj.images.fixed_width.width
-            const height = obj.images.fixed_height.height
-            const title = obj.title
+            const url = obj.images.fixed_width.url;
+            const width = obj.images.fixed_width.width;
+            const height = obj.images.fixed_height.height;
+            const title = obj.title;
     
             resultsHTML += `<img 
                 class="item"
@@ -111,13 +111,13 @@ function search(query) {
                 width="${width}" 
                 height="${height}"
                 alt="${title}"
-                >`
+                >`;
         });
     
-        resultsEl.innerHTML = resultsHTML //to add to html
+        resultsEl.innerHTML = resultsHTML; //to add to html
         
     }).catch(function(err) {
-        console.log(err.message)
-    })
+        console.log(err.message);
+    });
 }
 
