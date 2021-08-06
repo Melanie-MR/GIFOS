681cffdd661ef001d7586a1d3625dcf2e5a48eb2
fotomelu <69889253+fotomelu@users.noreply.github.com>
2021-08-06 15:18:20 -0700

    trending copy

--- js/modal.js
+++ js/modal.js
@@ -1,16 +1,10 @@
 // Get the modal
 let modal = document.getElementById("myModal");
 
-
-
-
-
 // When the user calls this function. The modal opens up
 function showModal() {
     modal.style.display = "block";
 }
-
-
 
 
 //When the user clicks anywhere outside of the modal, close it
